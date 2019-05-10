8acf3ae9d819b374503e8b12b4a313be3f93584a
Noam Wies <noamwies@gmail.com>
2019-05-10 18:27:52 +0300

    more modular SimpleConvNetAutoregressive1D

--- src/pyket/machines/simple_conv_net_autoregressive_1D.py
+++ src/pyket/machines/simple_conv_net_autoregressive_1D.py
@@ -1,17 +1,47 @@
 from .abstract_machine import AutoNormalizedAutoregressiveMachine
 from ..layers import ToFloat32, DownShiftLayer, VectorToComplexNumber, WeightNormalization
 
-from tensorflow.keras.layers import Lambda, Conv1D, Reshape, ZeroPadding1D
+from tensorflow.keras.layers import Activation, Conv1D, Lambda, Layer, Reshape, ZeroPadding1D
 from tensorflow.keras import backend as K
 
 
+class CausalConv1D(Layer):
+    """docstring for CausalConv1D"""
+    def __init__(self, filters, kernel_size, weights_normalization, activation=None):
+        super(CausalConv1D, self).__init__()
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.activation = activation
+        self.weights_normalization = weights_normalization
+        self.padding = self.kernel_size - 1
+
+    def build(self, input_shape):
+        self.conv_layer = Conv1D(filters=self.filters, kernel_size=self.kernel_size, strides=1)
+        if self.weights_normalization:
+            self.conv_layer = WeightNormalization(self.conv_layer, data_init=False)
+        if self.padding > 0:
+            self.padding_layer = ZeroPadding1D(padding=(self.padding, 0))
+        self.activation_layer = Activation(self.activation)
+
+    def call(self, x, mask=None):
+        if self.padding > 0:
+            x = self.padding_layer(x)
+        x = self.conv_layer(x)
+        return self.activation_layer(x)
+
+    def get_config(self):
+        config = {'filters': self.filters, 'kernel_size': self.kernel_size, 
+                  'activation': self.activation, 'weights_normalization': self.weights_normalization}
+        base_config = super().get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+        
+
 class SimpleConvNetAutoregressive1D(AutoNormalizedAutoregressiveMachine):
     """docstring for ConvNetAutoregressive1D"""
-    def __init__(self, keras_input_layer, depth, num_of_channels, kernel_size=3, strides=1, activation='relu', weights_normalization=True, **kwargs):
+    def __init__(self, keras_input_layer, depth, num_of_channels, kernel_size=3, activation='relu', weights_normalization=True, **kwargs):
         self.depth = depth
         self.num_of_channels = num_of_channels
         self.kernel_size = kernel_size
-        self.strides = strides
         self.activation = activation
         self.weights_normalization = weights_normalization
         self._build_unnormalized_conditional_log_wave_function(keras_input_layer)
@@ -20,20 +50,17 @@ class SimpleConvNetAutoregressive1D(AutoNormalizedAutoregressiveMachine):
     @property
     def unnormalized_conditional_log_wave_function(self):
         return self._unnormalized_conditional_log_wave_function
-
+    
     def _build_unnormalized_conditional_log_wave_function(self, keras_input_layer):
         x = ToFloat32()(keras_input_layer)
         x = Lambda(lambda y:K.expand_dims(y, axis=-1))(x)
         for i in range(self.depth - 1):
-            conv_layer = Conv1D(filters=self.num_of_channels, kernel_size=self.kernel_size, padding='valid',
-                        strides=self.strides, activation=self.activation)
-            if self.weights_normalization:
-                conv_layer = WeightNormalization(conv_layer, data_init=False)
-            x = conv_layer(ZeroPadding1D(padding=(self.kernel_size - self.strides, 0))(x))
+            x = CausalConv1D(filters=self.num_of_channels, 
+                             kernel_size=self.kernel_size,
+                             activation=self.activation, 
+                             weights_normalization=self.weights_normalization)(x)
         x = DownShiftLayer()(x)
-        conv_layer = Conv1D(filters=4, kernel_size=1, padding='valid', strides=1)
-        if self.weights_normalization:
-            conv_layer = WeightNormalization(conv_layer, data_init=False)
-        x = conv_layer(x)
+        x = CausalConv1D(filters=4, kernel_size=1, 
+                         weights_normalization=self.weights_normalization)(x)
         x = Reshape((K.int_shape(keras_input_layer)[1], 2, 2))(x)
         self._unnormalized_conditional_log_wave_function = VectorToComplexNumber()(x)
