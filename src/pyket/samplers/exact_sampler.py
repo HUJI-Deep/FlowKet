c1e4a251f0db2aa2622e61f6acec5d92b1aebf99
Noam Wies <noamwies@gmail.com>
2019-05-15 19:25:06 +0300

    add test that compare sampler to exact sampling on small systems

--- src/pyket/samplers/exact_sampler.py
+++ src/pyket/samplers/exact_sampler.py
@@ -6,12 +6,13 @@ from ..exact.utils import decimal_array_to_binary_array
 
 class ExactSampler(Sampler):
     """docstring for Sampler"""
+
     def __init__(self, exact_variational, batch_size, **kwargs):
         super(ExactSampler, self).__init__(exact_variational.input_size, batch_size, **kwargs)
         self.exact_variational = exact_variational
-    
+
     def next_batch(self):
         self.batch = decimal_array_to_binary_array(
             numpy.random.choice(self.exact_variational.num_of_states,
                                 size=self.batch_size, p=self.exact_variational.probs),
-            self.exact_variational.number_of_spins).reshape((self.batch_size, )+self.input_size)
+            self.exact_variational.number_of_spins).reshape((self.batch_size,) + self.input_size)
---
+++ tests/test_samplers.py
@@ -0,0 +1,77 @@
+from pyket.exact.utils import binary_array_to_decimal_array
+from pyket.machines import SimpleConvNetAutoregressive1D, ConvNetAutoregressive2D
+from pyket.samplers import AutoregressiveSampler, ExactSampler, FastAutoregressiveSampler, MetropolisHastingsLocal
+from pyket.operators import Operator
+from pyket.optimization import ExactVariational
+
+import numpy
+import pytest
+import tensorflow
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+
+from tensorflow.python.keras import backend as K
+
+ONE_DIM_INPUT = Input(shape=(10,), dtype='int8')
+TWO_DIM_INPUT = Input(shape=(4, 3), dtype='int8')
+GRAPH = tensorflow.get_default_graph()
+BATCH_SIZE = 2 ** 10
+
+
+class IdentityOperator(Operator):
+    def __init__(self, hilbert_state_shape):
+        super(IdentityOperator, self).__init__(hilbert_state_shape)
+
+    def find_conn(self, sample):
+        all_conn = numpy.expand_dims(sample, axis=0)
+        mel = numpy.full((all_conn.shape[0], sample.shape[0]), 1.0)
+        return all_conn, mel, numpy.ones_like(mel, dtype=numpy.bool)
+
+
+def sampler_factory(sampler_class, machine, machine_input, num_of_samples):
+    if sampler_class == MetropolisHastingsLocal:
+        model = Model(inputs=machine_input, outputs=machine.predictions)
+    else:
+        model = Model(inputs=machine_input, outputs=machine.conditional_log_probs)
+    return sampler_class(model, num_of_samples, mini_batch_size=BATCH_SIZE)
+
+
+@pytest.mark.parametrize('sampler_class, machine_input, machine_class, machine_args', [
+    (AutoregressiveSampler, ONE_DIM_INPUT, SimpleConvNetAutoregressive1D,
+     {'depth': 5, 'num_of_channels': 16, 'weights_normalization': False}),
+    (AutoregressiveSampler, TWO_DIM_INPUT, ConvNetAutoregressive2D,
+     {'depth': 2, 'num_of_channels': 16, 'weights_normalization': False}),
+    (FastAutoregressiveSampler, ONE_DIM_INPUT, SimpleConvNetAutoregressive1D,
+     {'depth': 5, 'num_of_channels': 16, 'weights_normalization': False}),
+    (FastAutoregressiveSampler, ONE_DIM_INPUT, SimpleConvNetAutoregressive1D,
+     {'depth': 5, 'num_of_channels': 16, 'weights_normalization': True}),
+    (FastAutoregressiveSampler, TWO_DIM_INPUT, ConvNetAutoregressive2D,
+     {'depth': 2, 'num_of_channels': 16, 'weights_normalization': False}),
+    (MetropolisHastingsLocal, ONE_DIM_INPUT, SimpleConvNetAutoregressive1D,
+     {'depth': 5, 'num_of_channels': 16, 'weights_normalization': False}),
+    (MetropolisHastingsLocal, TWO_DIM_INPUT, ConvNetAutoregressive2D,
+     {'depth': 2, 'num_of_channels': 16, 'weights_normalization': False}),
+])
+def test_sampler_by_l2(sampler_class, machine_input, machine_class, machine_args):
+    # this test based on https://arxiv.org/pdf/1308.3946.pdf
+    with GRAPH.as_default():
+        machine = machine_class(machine_input, **machine_args)
+        model = Model(inputs=machine_input, outputs=machine.predictions)
+        operator = IdentityOperator(list(K.int_shape(machine_input)[1:]))
+        exact_variational = ExactVariational(model, operator, BATCH_SIZE)
+        exact_variational._update_wave_function_arrays()
+        exact_sampler = ExactSampler(exact_variational, BATCH_SIZE)
+        num_of_samples = max(2 ** 14, 8 * exact_variational.num_of_states)
+        exact_sampler.set_batch_size(num_of_samples, mini_batch_size=BATCH_SIZE)
+        exact_sampler.next_batch()
+        sampler = sampler_factory(sampler_class, machine, machine_input, num_of_samples)
+        sampler.next_batch()
+        sampler_chosen_idx = binary_array_to_decimal_array(sampler.batch.reshape((num_of_samples, -1)))
+        exact_sampler_chosen_idx = binary_array_to_decimal_array(exact_sampler.batch.reshape((num_of_samples, -1)))
+        x = numpy.bincount(sampler_chosen_idx.astype(numpy.int), minlength=exact_variational.num_of_states)
+        y = numpy.bincount(exact_sampler_chosen_idx.astype(numpy.int), minlength=exact_variational.num_of_states)
+        z = (numpy.square(x - y) - x - y).sum()
+        estimated_sampler_diff_l2_norm = numpy.sqrt(numpy.abs(z))
+        estimated_sampler_diff_l2_norm /= num_of_samples
+        test_noise = numpy.sqrt(1.0 / num_of_samples)
+        assert estimated_sampler_diff_l2_norm < test_noise
