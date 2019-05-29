6f6c3d6b1b9c3b17a9461aef1dfddafc21174ed8
Noam Wies <noamwies@gmail.com>
2019-05-29 14:06:41 +0300

    add test

--- tests/test_gradient_per_example.py
+++ tests/test_gradient_per_example.py
@@ -4,11 +4,11 @@ import tensorflow as tf
 
 from tensorflow.python.ops.parallel_for import gradients
 from tensorflow.keras.layers import Activation, Input, Conv1D, Conv2D, Conv2DTranspose, Dense, ZeroPadding1D, \
-    ZeroPadding2D, ZeroPadding3D, Flatten, AveragePooling1D, AveragePooling2D
+    ZeroPadding2D, ZeroPadding3D, Flatten, AveragePooling1D, AveragePooling2D, Lambda
 from tensorflow.keras.models import Model
 import keras.backend as K
 
-from pyket.layers import ExpandInputDim, ComplexConv1D, ComplexConv2D, ComplexDense, ToComplex64
+from pyket.layers import ExpandInputDim, ComplexConv1D, ComplexConv2D, ComplexDense, ToComplex64, PeriodicPadding
 from pyket.layers.complex.tensorflow_ops import lncosh
 from pyket.utils.jacobian import gradient_per_example
 
@@ -52,6 +52,19 @@ def complex_values_2d_model():
     return Model(input_layer, x)
 
 
+def complex_values_1d_model():
+    padding = ((0, 3),)
+    input_layer = Input(shape=(20, 1), dtype='int8')
+    x = ToComplex64()(input_layer)
+    for i in range(2):
+        x = PeriodicPadding(padding)(x)
+        x = ComplexConv1D(4, 4, use_bias=False)(x)
+        x = Activation(lncosh)(x)
+    x = Flatten()(x)
+    x = Lambda(lambda y: tf.reduce_sum(y, axis=1, keepdims=True))(x)
+    return Model(input_layer, x)
+
+
 def real_values_1d_model():
     input_layer = Input((7,))
     first_conv_layer = Conv1D(16, kernel_size=3, strides=1)
@@ -75,6 +88,7 @@ def real_values_1d_model():
     (real_values_2d_model, 5),
     (complex_values_2d_model, 5),
     (real_values_1d_model, 5),
+    (complex_values_1d_model, 5),
 ])
 def test_equal_to_builtin_jacobian(model_builder, batch_size):
     with DEFAULT_TF_GRAPH.as_default():
