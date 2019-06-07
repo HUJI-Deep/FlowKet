b6559e0edd8b7bbae00b82cdbd745bd05a92a058
Noam Wies <noamwies@gmail.com>
2019-06-07 18:24:14 +0300

    improve DependencyGraph implementation

--- src/pyket/samplers/fast_autoregressive/data_structures.py
+++ src/pyket/samplers/fast_autoregressive/data_structures.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 
 import numpy
 from tensorflow.python.keras.engine.input_layer import InputLayer
+from tqdm import tqdm
 
 GraphNode = namedtuple('GraphNode', ['layer', 'spatial_location'])
 Dependency = namedtuple('Dependency', ['input_index', 'spatial_location'])
@@ -10,7 +11,10 @@ NONE_INT_VALUE = -1
 
 
 class DependencyGraph(object):
-    """docstring for DependencyGraph"""
+    """docstring for DependencyGraph
+    we Need this custom class instead of existing graphs libraries such as networkx because in deep models (depth ~20)
+    theirs topological_sort is very slow (probably because of inefficient memory usage)
+    """
 
     def __init__(self, keras_model, default_max_number_of_outgoing_edges=10):
         super(DependencyGraph, self).__init__()
@@ -72,10 +76,11 @@ class DependencyGraph(object):
 
     def _get_spatial_index(self, vertex):
         res = vertex.spatial_location[-1]
+        prev_dim_factor = 1
         for prev_dim_size, dim_location in zip(self._layers_shape[vertex.layer][::-1][:-1],
                                                vertex.spatial_location[::-1][1:]):
-            res *= prev_dim_size
-            res += dim_location
+            prev_dim_factor *= prev_dim_size
+            res += dim_location * prev_dim_factor
         return res
 
     def _spatial_index_to_location(self, spatial_index, layer_index):
@@ -100,25 +105,48 @@ class DependencyGraph(object):
             from_vertex_spatial_index, edge_index] = to_vertex_layer_index
 
     def topological_sort(self):
+        print('start topological_sort')
         results = []
-        visited_nodes_conter = 0
+        visited_nodes_counter = 0
         self._reset_dfs_status()
-        self._add_zero_incoming_dgree_to_queue()
-        while self._queue_len > 0:
-            layer_index = self._queue_layer_index[self._queue_len]
-            layer = self.keras_model.layers[layer_index]
-            spatial_location = self._spatial_index_to_location(self._queue_spatial_index[self._queue_len], layer_index)
-            results.append(GraphNode(layer=layer, spatial_location=spatial_location))
-            visited_nodes_conter += 1
-            # todo decrease in dgree of node outgoing neighbors
-        if visited_nodes_conter != self.vertices_counter:
+        self._add_vertices_with_zero_incoming_degree_to_queue()
+        with tqdm(total=self.vertices_counter) as progress_bar:
+            while self._queue_len > 0:
+                layer_index = self._queue_layer_index[self._queue_len - 1]
+                layer = self.keras_model.layers[layer_index]
+                spatial_index = self._queue_spatial_index[self._queue_len - 1]
+                spatial_location = self._spatial_index_to_location(spatial_index, layer_index)
+                results.append(GraphNode(layer=layer, spatial_location=spatial_location))
+                visited_nodes_counter += 1
+                progress_bar.update(visited_nodes_counter)
+                self._queue_len -= 1
+                self._decrease_incoming_edge_counter_from_node_neighbors(layer, spatial_index)
+        if visited_nodes_counter != self.vertices_counter:
             raise Exception('Dependency graph has cycle, invalid autoregressive model')
+        print('finish topological_sort')
         return results
+
+    def _decrease_incoming_edge_counter_from_node_neighbors(self, layer, spatial_index):
+        for i in range(self._outgoing_edges_counter[layer][spatial_index]):
+            to_layer_index = self._outgoing_edges_layer_index[layer][spatial_index, i]
+            to_layer = self.keras_model.layers[to_layer_index]
+            to_spatial_index = self._outgoing_edges_spatial_index[layer][spatial_index, i]
+            self._incoming_edges_dfs_status[to_layer][to_spatial_index] -= 1
+            if self._incoming_edges_dfs_status[to_layer][to_spatial_index] == 0:
+                self._queue_layer_index[self._queue_len] = to_layer_index
+                self._queue_spatial_index[self._queue_len] = to_spatial_index
+                self._queue_len += 1
 
     def _reset_dfs_status(self):
         self._queue_len = 0
         for layer, incoming_edges_counter in self._incoming_edges_counter.items():
-            self._incoming_edges_dfs_status[layer[:] = incoming_edges_counter[:]
+            self._incoming_edges_dfs_status[layer][:] = incoming_edges_counter[:]
 
-    def _add_zero_incoming_dgree_to_queue(self):
-        pass
+    def _add_vertices_with_zero_incoming_degree_to_queue(self):
+        for layer, incoming_edges_counter in self._incoming_edges_dfs_status.items():
+            layer_index = self._layer_to_layer_index[layer]
+            for i in range(incoming_edges_counter.shape[0]):
+                if incoming_edges_counter[i] == 0:
+                    self._queue_layer_index[self._queue_len] = layer_index
+                    self._queue_spatial_index[self._queue_len] = i
+                    self._queue_len += 1
