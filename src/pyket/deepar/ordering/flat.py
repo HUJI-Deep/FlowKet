2d79217ce5cf9c24777bd1939fcf80a60cc415cf
Noam Wies <noamwies@gmail.com>
2019-06-26 15:36:56 +0300

    better flat ordering

--- src/pyket/deepar/ordering/flat.py
+++ src/pyket/deepar/ordering/flat.py
@@ -5,3 +5,10 @@ def to_flat_ordering(ordering, shape):
             current_idx *= dim_size
             current_idx += dim_idx
         yield current_idx
+
+
+def to_flat_inverse_ordering(ordering, shape):
+    inversed_order = [0] * len(ordering)
+    for i, p in enumerate(to_flat_ordering(ordering, shape)):
+        inversed_order[p] = i
+    return inversed_order
