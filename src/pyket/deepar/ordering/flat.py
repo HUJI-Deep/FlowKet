cc7ed5525f2544e0919fbd73559a87e2bc625228
Noam Wies <noamwies@gmail.com>
2019-06-26 15:07:28 +0300

    implement flatting given order

--- src/pyket/deepar/ordering/flat.py
+++ src/pyket/deepar/ordering/flat.py
@@ -1,4 +1,7 @@
-def to_flat_ordering(ordering):
+def to_flat_ordering(ordering, shape):
     for i in ordering:
-        yield
-    pass
+        current_idx = 0
+        for dim_idx, dim_size in zip(i, shape):
+            current_idx *= dim_size
+            current_idx += dim_idx
+        yield current_idx
