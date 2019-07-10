ff0dc61a41a04e2214ed4eea7bd561288d2a9e9b
Noam Wies <noamwies@gmail.com>
2019-07-10 18:02:05 +0300

    better __init__

--- src/pyket/deepar/graph_analysis/__init__.py
+++ src/pyket/deepar/graph_analysis/__init__.py
@@ -3,6 +3,7 @@ from .convolutional_topology import ConvolutionalTopology
 from .data_structures import Dependency, GraphNode
 from .dependency_graph import DependencyGraph
 from .gathering_topology import GatherTopology
+from .layer_topology import LayerTopology
 from .masking_topology import DownShiftTopology, RightShiftTopology
 from .one_to_one_topology import OneToOneTopology, OneToOneTopologyWithIdentity
 from .padding_topology import PaddingTopology
