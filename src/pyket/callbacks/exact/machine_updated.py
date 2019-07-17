c40963beb724c7481e73f69f57ae16994785907a
Noam Wies <noamwies@gmail.com>
2019-07-17 07:43:35 +0300

    support updating just the wave_function withoutthe energy (the energy update is very slow)

--- src/pyket/callbacks/exact/machine_updated.py
+++ src/pyket/callbacks/exact/machine_updated.py
@@ -2,15 +2,22 @@ from tensorflow.keras.callbacks import Callback
 
 
 class MachineUpdated(Callback):
-    def __init__(self, exact_variational, update_in_batch_or_epoch=True, **kwargs):
+    def __init__(self, exact_variational, update_in_batch_or_epoch=True, update_local_energy=True, **kwargs):
         super(MachineUpdated, self).__init__(**kwargs)
         self.exact_variational = exact_variational
         self.update_in_batch_or_epoch = update_in_batch_or_epoch
+        self.update_local_energy = update_local_energy
 
     def on_batch_end(self, batch, logs=None):
         if self.update_in_batch_or_epoch:
-            self.exact_variational.machine_updated()
+            if self.update_local_energy:
+                self.exact_variational.machine_updated()
+            else:
+                self.exact_variational._update_wave_function_arrays()
 
     def on_epoch_end(self, epoch, logs=None):
         if not self.update_in_batch_or_epoch:
-            self.exact_variational.machine_updated()
+            if self.update_local_energy:
+                self.exact_variational.machine_updated()
+            else:
+                self.exact_variational._update_wave_function_arrays()
