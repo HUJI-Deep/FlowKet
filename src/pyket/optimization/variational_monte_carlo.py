36ed7520045efbac4fa5d98fbfbe547c1704df3a
Noam Wies <noamwies@gmail.com>
2019-10-20 17:01:12 +0300

    fix ttf 1.10 support and better supporint mini batch

--- src/pyket/optimization/variational_monte_carlo.py
+++ src/pyket/optimization/variational_monte_carlo.py
@@ -5,17 +5,18 @@ import numpy
 import tensorflow
 from tensorflow.python.keras import backend as K
 
+from .mini_batch_generator import MiniBatchGenerator
 from ..observables.monte_carlo import Observable, BaseObservable
 
 
-class VariationalMonteCarlo(object):
+class VariationalMonteCarlo(MiniBatchGenerator):
     """docstring for VariationalMonteCarlo"""
 
     def __init__(self, model, operator, sampler, mini_batch_size=None):
-        super(VariationalMonteCarlo, self).__init__()
+        super(VariationalMonteCarlo, self).__init__(sampler.batch_size, mini_batch_size)
         self.model = model
         self.operator = operator
-        self.set_sampler(sampler, mini_batch_size)
+        self.sampler = sampler
         self._graph = tensorflow.get_default_graph()
         self._session = K.get_session()
         self.current_batch = None
@@ -26,10 +27,7 @@ class VariationalMonteCarlo(object):
 
     def set_sampler(self, sampler, mini_batch_size=None):
         self.sampler = sampler
-        self._batch_size = sampler.batch_size
-        if mini_batch_size is None:
-            mini_batch_size = sampler.batch_size
-        self._mini_batch_size = mini_batch_size
+        return self.set_batch_size(sampler.batch_size, mini_batch_size)
 
     def _update_batch_local_energy(self):
         self.current_energy, self.current_local_energy_variance, self.current_local_energy = \
@@ -39,7 +37,7 @@ class VariationalMonteCarlo(object):
         local_energy_minus_mean = self.current_local_energy - self.current_energy
         return numpy.conj(local_energy_minus_mean)
 
-    def next(self):
+    def next_batch(self):
         K.set_session(self._session)
         with self._graph.as_default():
             self.start_time = time.time()
@@ -47,14 +45,4 @@ class VariationalMonteCarlo(object):
             self.sampling_end_time = time.time()
             self._update_batch_local_energy()
             self.local_energy_end_time = time.time()
-            return self.current_batch, self.loss_coefficients() / self._batch_size
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return self.next()
-
-    def to_generator(self):
-        while True:
-            yield next(self)
+            return self.current_batch, self.loss_coefficients() / self.batch_size
---
+++ src/pyket/utils/v1_to_v2.py
@@ -0,0 +1,14 @@
+import tensorflow
+
+def fix_tensorflow_v1_names():
+    tensorflow.math.real = tensorflow.real
+    tensorflow.math.imag = tensorflow.imag
+    tensorflow.math.reduce_logsumexp = tensorflow.reduce_logsumexp
+    tensorflow.math.reduce_sum = tensorflow.reduce_sum
+    tensorflow.math.reduce_mean = tensorflow.reduce_mean 
+    tensorflow.math.reduce_max = tensorflow.reduce_max
+    tensorflow.math.reduce_min = tensorflow.reduce_min
+    tensorflow.math.multiply = tensorflow.multiply
+    tensorflow.math.sqrt = tensorflow.sqrt
+    tensorflow.math.floormod = tensorflow.floormod
+    tensorflow.math.abs = tensorflow.abs
