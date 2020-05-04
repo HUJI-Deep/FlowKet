90b1d14622750c12411eb603072aea7bdcca91f0
Noam Wies <noamwies@gmail.com>
2020-05-04 20:27:58 +0300

    add scripts & weights that repreduce the paper results

---
+++ experiments/heisenberg_runner.py
@@ -0,0 +1,30 @@
+import argparse
+from flowket.operators import Heisenberg
+
+from train import train, create_training_config_parser
+from run_evaluation import run, create_evaluation_config_parser
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='Heisenberg NAQS')
+    subparsers = parser.add_subparsers()
+    parser_train = subparsers.add_parser('train', help='train ising model')
+    parser_eval = subparsers.add_parser('eval', help='eval ising model')
+    parser_train.set_defaults(func=train)
+    parser_eval.set_defaults(func=run)
+    create_evaluation_config_parser(parser=parser_eval, depth=20, mini_batch_size=2**7, hilbert_state_shape=[10, 10])
+    create_training_config_parser(parser=parser_train, depth=20, mini_batch_size=2**11, num_epoch=[80, 370], batch_size=[2**10, 2**1], hilbert_state_shape=[10, 10], learning_rate=[1e-3,1e-3])
+    parser.add_argument('--use_pbc', dest='pbc', action='store_true')
+    parser.add_argument('--no_pbc', dest='pbc', action='store_false')
+    parser.set_defaults(pbc=False)
+    config = parser.parse_args()
+
+    operator = Heisenberg(hilbert_state_shape=config.hilbert_state_shape, pbc=config.pbc)
+    true_ground_state_energy = None
+    if (not config.pbc) and len(config.hilbert_state_shape) == 2 and config.hilbert_state_shape[0] == 10 and  config.hilbert_state_shape[1] == 10:
+        true_ground_state_energy = -251.4624
+    config.func(operator, config, true_ground_state_energy)
+    
+if __name__ == '__main__':
+    main()
+
