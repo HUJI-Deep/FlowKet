585a91d495d0d6e83d3a4af062928cd9b78e2da4
Noam Wies <noamwies@gmail.com>
2020-05-06 13:14:00 +0300

    add support for ema

--- src/flowket/optimizers/accumulate_gradient_optimizer.py
+++ src/flowket/optimizers/accumulate_gradient_optimizer.py
@@ -2,7 +2,7 @@ import tensorflow
 from tensorflow.keras import backend as K
 
 
-def convert_to_accumulate_gradient_optimizer(orig_optimizer, update_params_frequency, accumulate_sum_or_mean=True):
+def convert_to_accumulate_gradient_optimizer(orig_optimizer, update_params_frequency, accumulate_sum_or_mean=True, ema_decay=0):
     if update_params_frequency < 1:
         raise ValueError('update_params_frequency must be >= 1')
     print('update_params_frequency: %s' % update_params_frequency)
@@ -14,25 +14,38 @@ def convert_to_accumulate_gradient_optimizer(orig_optimizer, update_params_frequ
     orig_get_updates = orig_optimizer.get_updates
     orig_optimizer.accumulated_iterations = K.variable(0, dtype='int64', name='accumulated_iterations')
     orig_optimizer.update_params_frequency = K.variable(update_params_frequency, dtype='int64', name='update_params_frequency')
+    if ema_decay > 0:
+        orig_optimizer.total_iterations = K.variable(0, dtype=K.floatx(), name='total_iterations')
 
     def set_update_params_frequency(self, update_params_frequency):
         K.set_value(self.update_params_frequency, update_params_frequency)
+
+    def set_weights_ema(self):
+        return tensorflow.group(*[K.update(p, e_p / (1 - K.pow(ema_decay, self.total_iterations))) for e_p, p in zip(self.params_ema, self.params_for_ema_tracking)])
 
     def updated_get_gradients(self, loss, params):
         return self.accumulate_gradient_accumulators
 
     def updated_get_updates(self, loss, params):
-        self.accumulate_gradient_accumulators = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        self.accumulate_gradient_accumulators = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name=p.name + '_gradient_accumulators') for p in params]
+        if ema_decay > 0:
+            self.params_for_ema_tracking = params
+            self.params_ema = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name=p.name + '_ema') for p in params]
         updates_accumulated_iterations = K.update_add(self.accumulated_iterations, 1)
         new_grads = orig_get_gradients(loss, params)
         if not accumulate_sum_or_mean:
             new_grads = [g / K.cast(self.update_params_frequency, K.dtype(g)) for g in new_grads]
         self.updated_grads = [K.update_add(p, g) for p, g in zip(self.accumulate_gradient_accumulators, new_grads)]
 
+
         def update_function():
+            updated_params = pass
             with tensorflow.control_dependencies(orig_get_updates(loss, params)):
                 reset_grads = [K.update(p, K.zeros(K.int_shape(p), dtype=K.dtype(p))) for p in
-                               self.accumulate_gradient_accumulators]
+                                self.accumulate_gradient_accumulators]
+                if ema_decay > 0:
+                    reset_grads += [K.update_add(self.total_iterations, 1)]
+                    reset_grads += [K.update(e_p, (e_p * ema_decay) + (1 - ema_decay) * p) for e_p, p in zip(self.params_ema, updated_params)]
             return tensorflow.group(*(reset_grads + [updates_accumulated_iterations]))
 
         def just_store_function():
@@ -47,3 +60,4 @@ def convert_to_accumulate_gradient_optimizer(orig_optimizer, update_params_frequ
     orig_optimizer.get_gradients = updated_get_gradients.__get__(orig_optimizer, type(orig_optimizer))
     orig_optimizer.get_updates = updated_get_updates.__get__(orig_optimizer, type(orig_optimizer))
     orig_optimizer.set_update_params_frequency = set_update_params_frequency.__get__(orig_optimizer, type(orig_optimizer))
+    orig_optimizer.set_weights_ema = set_weights_ema.__get__(orig_optimizer, type(orig_optimizer))
--- tests/test_gradients_aggregation.py
+++ tests/test_gradients_aggregation.py
@@ -10,12 +10,12 @@ import pytest
 import tensorflow as tf
 
 
-def get_simple_linear_model(orig_optimizer, update_params_frequency, accumulate_sum_or_mean):
+def get_simple_linear_model(orig_optimizer, update_params_frequency, accumulate_sum_or_mean, ema_decay=0):
     inputs = Input(shape=(1,), dtype='float32')
     outputs = Dense(1, use_bias=False, kernel_initializer='ones')(inputs)
     model = Model(inputs=inputs, outputs=outputs)
     convert_to_accumulate_gradient_optimizer(orig_optimizer, update_params_frequency=update_params_frequency,
-                                             accumulate_sum_or_mean=accumulate_sum_or_mean)
+                                             accumulate_sum_or_mean=accumulate_sum_or_mean, ema_decay=ema_decay)
 
     def y_loss(y_true, y_pred):
         return K.mean(y_pred)
@@ -26,12 +26,15 @@ def get_simple_linear_model(orig_optimizer, update_params_frequency, accumulate_
     def get_sgd_iteration():
         return orig_optimizer.get_weights()[orig_optimizer.weights.index(orig_optimizer.iterations)].item()
 
+    def set_weights_ema():
+        orig_optimizer.set_weights_ema()
+
     model.compile(optimizer=orig_optimizer, loss=y_loss)
-    return model, get_w, get_sgd_iteration
+    return model, get_w, get_sgd_iteration, set_weights_ema
 
 
 def test_update_just_when_need():
-    model, get_w, get_sgd_iteration = get_simple_linear_model(SGD(lr=1.0), 2, False)
+    model, get_w, get_sgd_iteration, _ = get_simple_linear_model(SGD(lr=1.0), 2, False)
     w_before_call = get_w()
     model.fit(x=np.array([[2.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
     w_after_first_call = get_w()
@@ -48,7 +51,7 @@ def test_update_just_when_need():
 
 def test_changing_the_update_frequency():
     optimizer = SGD(lr=1.0)
-    model, get_w, get_sgd_iteration = get_simple_linear_model(optimizer, 1, False)
+    model, get_w, get_sgd_iteration, _ = get_simple_linear_model(optimizer, 1, False)
     optimizer.set_update_params_frequency(2)
     w_before_call = get_w()
     model.fit(x=np.array([[2.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
@@ -69,8 +72,39 @@ def test_changing_the_update_frequency():
 
 
 def test_reset_after_update():
-    model, get_w, get_sgd_iteration = get_simple_linear_model(SGD(lr=1.0), 1, False)
+    model, get_w, get_sgd_iteration, _ = get_simple_linear_model(SGD(lr=1.0), 1, False)
     model.fit(x=np.array([[2.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
     model.fit(x=np.array([[3.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
     w_after_second_call = get_w()
     assert w_after_second_call == -4.0
+
+
+def test_update_ema_just_when_need():
+    model, get_w, get_sgd_iteration, set_weights_ema = get_simple_linear_model(SGD(lr=1.0), 2, False, .9)
+    w_before_call = get_w()
+    model.fit(x=np.array([[2.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
+    w_after_first_call = get_w()
+    global_step_after_first_call = get_sgd_iteration()
+    model.fit(x=np.array([[3.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
+    w_after_second_call = get_w()
+    set_weights_ema()
+    w_after_set_ema = get_w()
+    global_step_after_second_call = get_sgd_iteration()
+    assert global_step_after_first_call == 0
+    assert global_step_after_second_call == 1
+    assert w_before_call == 1.0
+    assert w_after_first_call == 1.0
+    assert w_after_second_call == -1.5
+    assert w_after_set_ema == -1.5
+
+
+def test_ema():
+    model, get_w, get_sgd_iteration, set_weights_ema = get_simple_linear_model(SGD(lr=1.0), 1, False, .9)
+    model.fit(x=np.array([[2.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
+    model.fit(x=np.array([[3.0]], dtype=np.float32), y=np.array([[0.0]], dtype=np.float32), batch_size=1)
+    w_after_second_call = get_w()
+    set_weights_ema()
+    w_after_set_ema = get_w()
+    assert w_after_first_call == -1.0
+    assert w_after_second_call == -4.0
+    assert w_after_set_ema == pytest.approx(-2.5789)
